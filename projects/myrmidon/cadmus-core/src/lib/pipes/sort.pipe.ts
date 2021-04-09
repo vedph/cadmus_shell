c754201fe4604dcc782d20e687647758914b3185
Myrmex <dfusi@hotmail.com>
2021-04-09 15:59:07 +0200

    Fix to sort pipe

--- projects/myrmidon/cadmus-core/src/lib/pipes/sort.pipe.ts
+++ projects/myrmidon/cadmus-core/src/lib/pipes/sort.pipe.ts
@@ -9,6 +9,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SortPipe implements PipeTransform {
   transform(value: any[], propName: string): unknown {
     return value.sort((a, b) => {
+      if (!a && !b) {
+        return 0;
+      }
+      if (!a && b) {
+        return -1;
+      }
+      if (a && !b) {
+        return 1;
+      }
       if (a[propName] < b[propName]) {
         return -1;
       } else if (a[propName] === b[propName]) {
