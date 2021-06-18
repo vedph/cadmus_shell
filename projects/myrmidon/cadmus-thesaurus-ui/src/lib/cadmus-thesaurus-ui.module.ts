82f1908785e36f8cc6d5f014ede9a0db3029a322
Myrmex <dfusi@hotmail.com>
2021-06-18 23:44:26 +0200

    Removed unused deps from thesaurus ui

--- projects/myrmidon/cadmus-thesaurus-ui/src/lib/cadmus-thesaurus-ui.module.ts
+++ projects/myrmidon/cadmus-thesaurus-ui/src/lib/cadmus-thesaurus-ui.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CadmusMaterialModule } from '@myrmidon/cadmus-material';
-import { CadmusStateModule } from '@myrmidon/cadmus-state';
 import { CadmusUiModule } from '@myrmidon/cadmus-ui';
 import { ThesaurusEditorComponent } from './components/thesaurus-editor/thesaurus-editor.component';
 import { ThesaurusLookupComponent } from './components/thesaurus-lookup/thesaurus-lookup.component';
@@ -16,7 +15,6 @@ import { ThesaurusNodeComponent } from './components/thesaurus-node/thesaurus-no
     // cadmus
     CadmusMaterialModule,
     CadmusUiModule,
-    CadmusStateModule,
   ],
   declarations: [
     ThesaurusEditorComponent,
